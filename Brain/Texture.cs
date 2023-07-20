5a7336cb47a96fe3e2d8c4405b9de4860b36dfe6
Galeev LXIX <gtimur477@gmail.com>
2023-07-21 02:49:27 +0500

    +

--- Brain/Texture.cs
+++ Brain/Texture.cs
@@ -3,23 +3,23 @@ using System.Drawing;
 using PixelFormat = OpenTK.Graphics.OpenGL.PixelFormat;
 using System.IO;
 using StbImageSharp;
+using System.Reflection.Metadata;
 
 namespace game_2.Brain
 {
     public class Texture
     {
-        public readonly int Handle;
+        private readonly int Handle;
 
-        public Texture(int glHandle)
+        private Texture(int glHandle)
         {
             Handle = glHandle;
         }
 
         public static Texture Load(string file_name)
         {
-            int handle = GL.GenTexture();   //Создаем
-
-            GL.ActiveTexture(TextureUnit.Texture0);
+            int handle = GL.GenTexture();
+            
             GL.BindTexture(TextureTarget.Texture2D, handle);
 
             StbImage.stbi_set_flip_vertically_on_load(1);
@@ -28,23 +28,26 @@ namespace game_2.Brain
             {
                 ImageResult image = ImageResult.FromStream(stream, ColorComponents.RedGreenBlueAlpha);
                 GL.TexImage2D(
-                    TextureTarget.Texture2D,    //target
-                    0,                          //level
-                    PixelInternalFormat.Rgba,   
-                    image.Width, 
-                    image.Height, 
-                    0,                          //border
-                    PixelFormat.Rgba, 
-                    PixelType.UnsignedByte, 
-                    image.Data);
+                    TextureTarget.Texture2D,    //Тип создаваемой текстуры
+                    0,                          //Уровень детализации
+                    PixelInternalFormat.Rgba,   //Формат для хранения пикселей на графическом процессоре
+                    image.Width,                //Ширина изображения
+                    image.Height,               //Высота изображения
+                    0,                          //Граница изображения
+                    PixelFormat.Rgba,           //Формат байтов   
+                    PixelType.UnsignedByte,     //Тип пикселей
+                    image.Data);                //Массив пикселей
             }
 
+            //Фильтрация
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
 
+            //Перенос
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapS, (int)TextureWrapMode.Repeat);
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureWrapT, (int)TextureWrapMode.Repeat);
 
+            //генерируем коллекцию mipmapped
             GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
 
             return new Texture(handle);
--- Shaders/FragmentShader.hlsl
+++ Shaders/FragmentShader.hlsl
@@ -8,4 +8,4 @@ uniform sampler2D texture0;
 void main() 
 { 
 	outputColor = texture(texture0, texCoord);
-}             
+}
